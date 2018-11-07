3b6f0f1c35b6786f58e2b18a2ab7f1d4272eeff0
ka-zu <sprite13return@gmail.com>
2018-11-08 02:15:43 +0900

    波形読み取り準備 フォームデザインと必要変数の定義

--- ProjectionMapping/Form1.Designer.cs
+++ ProjectionMapping/Form1.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.plotView2 = new OxyPlot.WindowsForms.PlotView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(32, 88);
+            this.button1.Location = new System.Drawing.Point(34, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 86);
             this.button1.TabIndex = 0;
@@ -44,7 +49,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(161, 89);
+            this.button2.Location = new System.Drawing.Point(166, 13);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(89, 85);
             this.button2.TabIndex = 1;
@@ -52,17 +57,76 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(12, 175);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(260, 80);
+            this.plotView1.TabIndex = 2;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.plotView1.Click += new System.EventHandler(this.plotView1_Click);
+            // 
+            // plotView2
+            // 
+            this.plotView2.Location = new System.Drawing.Point(12, 297);
+            this.plotView2.Name = "plotView2";
+            this.plotView2.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView2.Size = new System.Drawing.Size(260, 80);
+            this.plotView2.TabIndex = 3;
+            this.plotView2.Text = "plotView2";
+            this.plotView2.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView2.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView2.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.plotView2.Click += new System.EventHandler(this.plotView2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "波形";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "周波数ヒストグラム";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 389);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plotView2);
+            this.Controls.Add(this.plotView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +134,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private OxyPlot.WindowsForms.PlotView plotView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ProjectionMapping/Form1.cs
+++ ProjectionMapping/Form1.cs
@@ -9,6 +9,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 /*
+ * NAudioの入れ方
+ * Nugetマネージャで
+ * NAudioで検索して入れる
+ */
+using NAudio.Wave;//NAudio用
+
+/*
  * OpenTKの入れ方
  * Nugetマネージャで
  * OpenTK
@@ -24,16 +31,95 @@ using System.Windows.Forms;
  * ソリューションと同じ階層のpackages内の
  * OpenTK.GLControl.3.0.1\lib\net20内のOpenTK.GLControl.dll
  * を参照する
-*/
+ */
 using OpenTK;
+
+/*
+ * OxyPlotの入れ方
+ * Nugetマネージャで
+ * OxyPlot.Core
+ * OxyPlot.WindowsForm
+ * を入れる
+ * OxyPlot.Core.1.0.0\lib\net45内のOxyPlot.dllを
+ * OxyPlot.WindowsForms.1.0.0\lib\net45にコピーしておく
+ * 
+ * プロットモデルの出し方
+ * ツールボックス右クリック->アイテムの選択で
+ * ソリューションと同じ階層のpackages内の
+ * OxyPlot.WindowsForms.1.0.0\lib\net45内のOxyPlot.WindowsForms.dll
+ * を参照する
+ */
+using OxyPlot;
+using OxyPlot.Axes;//プロット軸用
+using OxyPlot.Series;
 
 namespace ProjectionMapping
 {
     public partial class Form1 : Form
     {
+        WaveIn waveIn;
+
+        //子のフォームが開いていたら
+        bool form2Opened = false;
+
+        //周波数ヒストグラムの最大値
+        double maxPow = 0;
+        //最大値のindex
+        int maxIndex = 0;
+        //そのindexに対応する周波数
+        double maxHz = 0;
+
+        //一度の表示する点の数
+        int samplingNum = 256;
+
+        //グラフ用変数
+        private PlotModel _plotmodel = new PlotModel();
+        private PlotModel _plotmodel2 = new PlotModel();
+
+        //プロットの軸
+        private LinearAxis linearAxis1 = new LinearAxis
+        {
+            Position = AxisPosition.Bottom
+        };
+        private LinearAxis _linearAxis2 = new LinearAxis
+        {
+            Minimum = -1.0,
+            Maximum = 1.0,
+            Position = AxisPosition.Left
+        };
+
+        private LinearAxis _linearAxis3 = new LinearAxis
+        {
+            Position = AxisPosition.Bottom
+        };
+        private LinearAxis _linearAxis4 = new LinearAxis
+        {
+            Maximum = 25.0,
+            Position = AxisPosition.Left
+        };
+
+        //たぶん格納した値がプロットされる
+        private LineSeries _linerSeries = new LineSeries();//波形用
+        private LineSeries _linerSeries2 = new LineSeries();//周波数用
+
+        //音声データ
+        List<float> _recorded = new List<float>(); //波形用
+        List<float> _recorded2 = new List<float>(); //周波数ヒストグラム用
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //デバイスを探してひょうじ
+            for(int i=0; i<WaveIn.DeviceCount; i++)
+            {
+                var deviceInfo = WaveIn.GetCapabilities(i);
+                this.label1.Text = String.Format("Device {0}: {1}, {2} cannels",
+                    i, deviceInfo.ProductName, deviceInfo.Channels);
+            }
         }
 
         //画像に合わせて線をクリックでなぞる画面
@@ -52,10 +138,35 @@ namespace ProjectionMapping
             f3.Show();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        
+
+        private void plotView2_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void plotView1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //******
+        //******自作関数
+        //******
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            //32bitで最大値1.0fとする
+            for(int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((e.Buffer[index + 1] << 8) | e.Buffer[index + 0]);
+                float sample32 = sample / 32768f;
+                //ProcessSample(sample32);
+                //ProcessSample2(sample32);
+            }
+        }
+
+
     }
     
 }
