da2177984c5a0bd6e1bd7daee918f0745835980f
ka-zu <sprite13return@gmail.com>
2018-11-08 01:34:01 +0900

    OpenGL＋音響の表示テストフォームを作成 等間隔に線を表示する部分の作成

---
+++ ProjectionMapping/Form3.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ProjectionMapping
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(285, 261);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+    }
+}
---
+++ ProjectionMapping/Form3.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenTK.Graphics.OpenGL;
+
+//音で上下するバーを投影する部分
+namespace ProjectionMapping
+{
+    public partial class Form3 : Form
+    {
+        //ラインの太さ
+        int lineWidth = 5; 
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            //画面を消す
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //テクスチャを使えるように
+            GL.Enable(EnableCap.Texture2D);
+
+            reshape(glControl1.Width, glControl1.Height);
+
+            GL.LineWidth(lineWidth);
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+
+            //画面を消す
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //線を引く
+            //drawLines();
+
+            
+
+            
+        }
+
+        //******
+        //******自作関数
+        //******
+
+        //GL表示座標系の変換
+        private void reshape(int wid, int hei)
+        {
+            int top = hei / 2;
+            int bottom = -hei / 2;
+            int left = -wid / 2;
+            int right = wid / 2;
+            GL.Viewport(new Rectangle(0, 0, wid, hei));
+            GL.LoadIdentity();
+            GL.Ortho(left, right, bottom, top, -1.0, 1.0);
+        }
+
+        //一定の間隔・長さのラインを引く
+        private void drawLines()
+        {
+            //引くラインの数
+            int num = 30;
+            //ライン間のマージン
+            int margin = lineWidth+5;
+            //ラインの長さ
+            int len = 50;
+            //ラインの左端の位置座標　-1 * 太さ * (ラインの数/2)
+            double left = -((num-1)/2.0)*margin;
+
+            //波打つように表示するテスト用
+            double[] sin = new double[30];
+            for(int i =0; i < sin.Length; i++)
+            {
+                sin[i] = 50 * Math.Sin((i * 10) * Math.PI / 180);
+            }
+
+            GL.Color4(OpenTK.Graphics.Color4.Red);
+            GL.Begin(BeginMode.Lines);
+
+            for (int i = 0; i < num; i++)
+            {
+                GL.Vertex2(left + (i * margin), 0);
+                GL.Vertex2(left + (i * margin), sin[i]);
+            }
+
+            GL.End();
+        }
+
+
+        //絵かきはこの中
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            /*GL.Color4(OpenTK.Graphics.Color4.Red);
+            GL.Begin(BeginMode.Lines);
+            GL.Vertex2(0, 0);
+            GL.Vertex2(0, 50);
+            GL.End();*/
+
+            drawLines();
+
+            //表示
+            glControl1.SwapBuffers();
+        }
+    }
+}
