c2f78a41b1640d88443134ca52370eb27157e713
ka-zu <sprite13return@gmail.com>
2018-11-10 06:32:02 +0900

    左画面投影用のフォーム

---
+++ ProjectionMapping/Form5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectionMapping
+{
+    /// <summary>
+    /// 左窓投影ウィンドウ
+    /// </summary>
+    public partial class Form5 : Form
+    {
+        //窓ポリゴン座標格納用
+        List<Point>[] winLists = new List<Point>[12];
+
+        public Form5()
+        {
+            InitializeComponent();
+        }
+    }
+}
